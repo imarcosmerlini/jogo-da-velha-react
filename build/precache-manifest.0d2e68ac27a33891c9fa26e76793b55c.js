ae934a0937de3e79fa27f5acf3cb95ea2fef89c2
Marcos Merlini <merlinimarcos@gmail.com>
2019-06-05 00:08:15 -0300

    Jogo base terminado

---
+++ build/precache-manifest.0d2e68ac27a33891c9fa26e76793b55c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "334fedb70fd014bf73b3d0fc8ecdd667",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6bbe9bfdf496a0e05201",
+    "url": "/static/css/main.3fa7586b.chunk.css"
+  },
+  {
+    "revision": "685d0c597e8419129599",
+    "url": "/static/js/2.0e3f6a1a.chunk.js"
+  },
+  {
+    "revision": "6bbe9bfdf496a0e05201",
+    "url": "/static/js/main.c6eb4549.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
